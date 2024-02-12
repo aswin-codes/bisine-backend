f3d078c31eeea2c1ac61642343693485d38ab26d
aswin-codes <codicon.co@gmail.com>
2024-02-12 23:20:10 +0530

    Database Configured

--- db.js
+++ db.js
@@ -1,14 +1,42 @@
 const { Pool } = require('pg');
 
-const credentials = {
-    user : 'postgres',
-    host : 'localhost',
-    database: 'bisine',
-    password: 'aswin',
-    port: 5432
-}
+// const credentials = {
+//     user : 'postgres',
+//     host : 'localhost',
+//     database: 'bisine',
+//     password: 'aswin',
+//     port: 5432
+// }
 
-const pool = new Pool(credentials);
+// const pool = new Pool(credentials);
+
+const pool = new Pool({
+    user: 'aswinraaj2405',
+    host: 'ep-yellow-shape-36279017-pooler.ap-southeast-1.aws.neon.tech',
+    database: 'bisine',
+    password: '8TCxeBGHVOP3',
+    port: 5432,
+    ssl: {rejectUnauthorized: false},
+    connect: {
+      options: `project=ep-yellow-shape-36279017-pooler`,
+    }// Default PostgreSQL port
+    
+  });
+  
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.log(err);
+      done();
+      return;
+    }
+  
+    console.log('Connected to database!');
+    done();
+  });
+  
+  
+  
+  module.exports = pool;
 
 
 module.exports = pool;
