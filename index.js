ff4a9238e67b195d28f425b685e9bb7af99da332
aswin-codes <codicon.co@gmail.com>
2024-04-16 17:51:11 +0530

    fix: cors

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ server.get('/test', async (req,res) => {
     })
 });
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://bisinevite.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 server.use("/api/user",userRouter);
 server.use("/api/shop",shopRouter);
 server.use("/api/product",productRouter);
