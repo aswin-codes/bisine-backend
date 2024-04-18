1ac74258885fe92ef7ffb4f2f36d7f8be1bd7611
aswin-codes <codicon.co@gmail.com>
2024-04-18 16:39:53 +0530

    fix : cors options

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 dotenv.config();
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://bisinevite.vercel.app'],
+    origin: ['http://localhost:5173','https://bisinevite.vercel.app'],
     credentials: true ,// Enable credentials (cookies, authorization headers, etc.)
     methods: ["PUT","DELETE","POST","GET"],
     allowedHeaders: ['Content-Type', 'Authorization']
