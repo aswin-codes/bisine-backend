c5652dc3201181b6509e3b6443171ea69a51c337
aswin-codes <aswinraaj.ps@gmail.com>
2024-11-06 20:15:29 +0530

    cors

--- db.js
+++ db.js
@@ -1,41 +1,41 @@
-const { Pool } = require('pg');
+  const { Pool } = require('pg');
 
-// const credentials = {
-//     user : 'postgres',
-//     host : 'localhost',
-//     database: 'bisine',
-//     password: 'aswin',
-//     port: 5432
-// }
+  // const credentials = {
+  //     user : 'postgres',
+  //     host : 'localhost',
+  //     database: 'bisine',
+  //     password: 'aswin',
+  //     port: 5432
+  // }
 
-// const pool = new Pool(credentials);
+  // const pool = new Pool(credentials);
 
-const pool = new Pool({
-    user: 'aswinraaj2405',
-    host: 'ep-yellow-shape-36279017-pooler.ap-southeast-1.aws.neon.tech',
-    database: 'bisine',
-    password: '8TCxeBGHVOP3',
-    port: 5432,
-    ssl: {rejectUnauthorized: false},
-    connect: {
-      options: `project=ep-yellow-shape-36279017-pooler`,
-    }// Default PostgreSQL port
+  const pool = new Pool({
+      user: 'postgres',
+      host: 'database-1.cb6gyskgg2ks.ap-south-1.rds.amazonaws.com',
+      database: 'postgres',
+      password: 'aswin2405',
+      port: 5432,
+      ssl: {rejectUnauthorized: false},
+      connect: {
+        options: `project=ep-yellow-shape-36279017-pooler`,
+      }// Default PostgreSQL port
+      
+    });
     
-  });
-  
-  pool.connect((err, client, done) => {
-    if (err) {
-      console.log(err);
+    pool.connect((err, client, done) => {
+      if (err) {
+        console.log(err);
+        done();
+        return;
+      }
+    
+      console.log('Connected to database!');
       done();
-      return;
-    }
-  
-    console.log('Connected to database!');
-    done();
-  });
-  
-  
+    });
+    
+    
 
 
 
-module.exports = pool;
+  module.exports = pool;
